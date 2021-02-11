555d56df5d4ddd9a7e3b1357a8b0d80af8945f97
daniel21h <hesseldaniel21@gmail.com>
2021-02-11 09:19:21 -0300

    feat: pipe data validation and manipulation

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,8 +1,21 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class User {
   id: number;
+
+  @IsNotEmpty({ message: 'The username is mandatory.' })
+  @IsString({ message: 'Username must be a string.' })
   username: string;
+
+  @IsNotEmpty({ message: 'The email is mandatory.' })
+  @IsEmail({}, { message: 'Invalid email.' })
   email: string;
+
+  @IsNotEmpty({ message: 'The password is mandatory.' })
   password: string;
+
+  @IsNotEmpty({ message: 'The name is mandatory.' })
   name: string;
+
   createdAt: Date;
 }
